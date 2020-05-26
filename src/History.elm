41e8799cf18d9a59040654f526e0bd3d6ea464c0
suxxus <suxxus@outlook.com>
2020-05-26 18:43:39 +0200

    Manage steps history using List

--- src/History.elm
+++ src/History.elm
@@ -1,21 +1,28 @@
-module History exposing (..)
+module History exposing
+    ( JourneyHistory
+    , getCurrentStep
+    , journeyHistory
+    , updateCurrentStep
+    )
 
 import Journey as Journey
 
 
-type alias JourneyHistory =
-    { visitedSteps : List Journey.Step
-    , currentStep : Journey.Step
-    , remainingSteps : List Journey.Step
-    }
+type JourneyHistory
+    = JourneyHistory
+        { visitedSteps : List Journey.Step
+        , currentStep : Journey.Step
+        , remainingSteps : List Journey.Step
+        }
 
 
-jouneryHistory : JourneyHistory
-jouneryHistory =
-    { visitedSteps = []
-    , currentStep = Journey.start
-    , remainingSteps = [ Journey.building ]
-    }
+journeyHistory : JourneyHistory
+journeyHistory =
+    JourneyHistory
+        { visitedSteps = []
+        , currentStep = Journey.start
+        , remainingSteps = [ Journey.building ]
+        }
 
 
 getNextStepOrDefault : Journey.Step -> List Journey.Step -> Journey.Step
@@ -28,9 +35,41 @@ getNextStepOrDefault defaultSep listOfSteps =
             a
 
 
+updateVisitedSteps : Journey.Step -> List Journey.Step -> List Journey.Step
+updateVisitedSteps step visitedSteps =
+    let
+        _ =
+            Debug.log "is member" step
+    in
+    if List.member step visitedSteps then
+        visitedSteps
+
+    else
+        step :: visitedSteps
+
+
 updateJourneyHistory : JourneyHistory -> JourneyHistory
 updateJourneyHistory history =
-    { visitedSteps = history.currentStep :: history.visitedSteps
-    , currentStep = getNextStepOrDefault history.currentStep history.remainingSteps
-    , remainingSteps = List.drop 1 history.remainingSteps
-    }
+    case history of
+        JourneyHistory record ->
+            JourneyHistory
+                { visitedSteps = updateVisitedSteps record.currentStep record.visitedSteps
+                , currentStep = getNextStepOrDefault record.currentStep record.remainingSteps
+                , remainingSteps = List.drop 1 record.remainingSteps
+                }
+
+
+updateCurrentStep : Journey.Step -> JourneyHistory -> JourneyHistory
+updateCurrentStep step history =
+    case history of
+        JourneyHistory record ->
+            JourneyHistory
+                { record
+                    | currentStep = step
+                }
+                |> updateJourneyHistory
+
+
+getCurrentStep : JourneyHistory -> Journey.Step
+getCurrentStep (JourneyHistory record) =
+    record.currentStep
